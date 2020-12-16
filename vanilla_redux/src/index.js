314ba1dfa26534c622bd009eeaf2e50cd8e597f5
Chae Sang, Jung <shtingstar@hanmail.net>
2020-12-17 00:11:39 +0900

    #1.1 Store and Reducer comments

--- vanilla_redux/src/index.js
+++ vanilla_redux/src/index.js
@@ -4,10 +4,15 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+// reducer
 const countModifier = (count = 0) => {
+  // returning state
   return count;
 };
 
+// createStore
 const countStore = createStore(countModifier);
 
+
+// console.log(countStore);
 console.log(countStore.getState());
