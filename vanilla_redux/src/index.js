7354fd7808f74b4301c7ef24664b48740998407c
Chae Sang, Jung <shtingstar@hanmail.net>
2020-12-17 00:50:24 +0900

    #1.3 Subscriptions

--- vanilla_redux/src/index.js
+++ vanilla_redux/src/index.js
@@ -4,9 +4,11 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 // reducer
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
+  // console.log(count, action);
   if(action.type === "ADD"){
     return count += 1;
   } else if(action.type === "MINUS") {
@@ -19,11 +21,25 @@ const countModifier = (count = 0, action) => {
 // createStore
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-
 // console.log(countStore);
 console.log(countStore.getState());
+
+const onTextUpdate = () => {
+  // console.log("there was a change on the store");
+  // console.log(countStore.getState())
+  number.innerText = countStore.getState();
+
+}
+
+countStore.subscribe(onTextUpdate);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
