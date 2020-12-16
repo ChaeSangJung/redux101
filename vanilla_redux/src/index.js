38eebe9beeacd3faf79c0e0f36a44e51c05a1036
Chae Sang, Jung <shtingstar@hanmail.net>
2020-12-16 23:57:16 +0900

    #1.1 Store and Reducer

--- vanilla_redux/src/index.js
+++ vanilla_redux/src/index.js
@@ -1,25 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const countModifier = (count = 0) => {
+  return count;
+};
 
-number.innerText = count;
+const countStore = createStore(countModifier);
 
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count += 1;
-  updateText();
-  
-}
-
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
