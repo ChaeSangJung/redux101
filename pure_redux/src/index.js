c5623cab645e5dd6e1ff359001e005c22ebdeadd
CheSangJung <shtingstar@hanmail.net>
2020-12-23 11:09:27 +0900

    #2.2 Delete To Do

--- pure_redux/src/index.js
+++ pure_redux/src/index.js
@@ -6,11 +6,25 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addTodo = (text) => {
+  return {
+    type: ADD_TODO, 
+    text: text
+  }
+}
+
+const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id: id
+  }
+}
+
 const reducer = (state=[], action) => {
-  console.log(action);
+  // console.log(action);
   switch(action.type){
     case ADD_TODO:
-      return [...state, {text: action.text, id: Date.now()}];
+      return [{text: action.text, id: Date.now()}, ...state];
       // return state.push(action.text); 안된다. 새로운 object를 만들어서 return을 해야 한다.
     case DELETE_TODO:
       return [];
@@ -19,11 +33,38 @@ const reducer = (state=[], action) => {
   }
 }
 
-const stroe = createStore(reducer);
+const store = createStore(reducer);
 
-stroe.subscribe(() => {
-  console.log(stroe.getState());
+store.subscribe(() => {
+  console.log(store.getState());
 });
+
+const dispatchAddToDo = text => {
+  store.dispatch(addTodo(text));
+}
+
+const dispatchDeleteToDo = (event) => {
+  const id = event.target.parentNode.id;
+  console.log("delete", id);
+  store.dispatch(deleteTodo(id));
+}
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);    
+  });
+}
+
+store.subscribe(paintToDos);
 
 // const createToDo = toDo => {
 //   const li = document.createElement("li");
@@ -34,8 +75,8 @@ stroe.subscribe(() => {
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
-  input.value = "";
-  stroe.dispatch({ type: ADD_TODO, text: toDo });
+  input.value = "";  
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
