c4067b71e612e7a22a6e09be06678a21f63d492c
Chae Sang, Jung <shtingstar@hanmail.net>
2020-12-26 23:41:03 +0900

    #4.2 createReducer re

--- redux_toolkit/src/components/ToDo.js
+++ redux_toolkit/src/components/ToDo.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
 function ToDo({ text, onBtnClick, id }) {
-    return (
-        <li>
-            <Link to={`/${id}`}>{text}</Link>
-            <button onClick={onBtnClick}>DEL</button>
-        </li>
-    );
+  return (
+    <li>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
+    </li>
+  );
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
-    // console.log(ownProps);
-    return {
-        onBtnClick: () => dispatch(actionCreators.deleteTodo(ownProps.id))
-    };
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+  };
 }
 
 export default connect(null, mapDispatchToProps)(ToDo);
--- redux_toolkit/src/routes/Home.js
+++ redux_toolkit/src/routes/Home.js
@@ -1,69 +1,42 @@
-// export default () => "Home";
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
 import ToDo from "../components/ToDo";
 
-
-// function Home({ toDos, dispatch }){    
-// function Home({ toDos, ...rest }){
-    // console.log(rest);
-function Home({ toDos, addTodo }){    
-    const [text, setText] = useState("");
-    function onChange(e) {
-        setText(e.target.value);
-    }
-    function onSubmit(e) {
-        e.preventDefault();
-        addTodo(text);
-        setText("");
-        // dispatch(addTodo(text));
-        // console.log(text);                
-    }
-    return (
-        <>
-            <h1>To Do</h1>
-            <form onSubmit={onSubmit}>
-                <input type="text" value={text} onChange={onChange} />
-                <button>add</button>
-            </form>
-            {/* 1. store에서 state를 가져올 수 있도록 해야 됨 */}
-            <ul>
-                {toDos.map((toDo) => (
-                    <ToDo {...toDo} key={toDo.id} />
-                ))}
-            </ul>
-        </>
-    )
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    addToDo(text);
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map(toDo => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </>
+  );
 }
 
-// redux store로부터 옴
-
-// state를 받아다가 components에 전달
-// Redux state로부터 home(component)에 prop로써 전달
-// componet에 render가 가능
-// function mapStateToProps(state, ownProps){
-function mapStateToProps(state){
-    // console.log(state, ownProps);
-    // connect()는 Home으로 보내는 props에 추가될 수 있도록 허용
-    // return { sexy: true };
-    return { toDos: state }
+function mapStateToProps(state) {
+  return { toDos: state };
 }
 
-// function mapDispatchToProps(dispatch, ownProps) {
 function mapDispatchToProps(dispatch) {
-    // console.log(dispatch);
-    // return { dispatch };
-    return {
-        addTodo: (text) => dispatch(actionCreators.addTodo(text))
-    };
+  return {
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  };
 }
 
-// 2. connect : componets(여기에서는 Home.js)들을 store에 연결 시켜줌
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
-// export default connect(null, mapDispatchToProps)(Home);
-
-// getState :  state를 전달
-// dispatch : store 혹은 reducer에 메시지 전달
-// todo 추가 => 
-// todo 가져오기 =>
